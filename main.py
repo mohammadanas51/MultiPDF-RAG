76955287dcd9f05191ce8874ba28abf7ef7f7aed
mohammadanas51 <mohammad.anas51@hotmail.com>
2026-06-22 17:56:58 +0530

    Hopefully it should work now

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain_classic.chains.question_answering import load_qa_chain
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import io
